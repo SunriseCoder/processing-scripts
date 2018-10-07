8a280d7cbc49a8643fab39f34b61841f9af645a2
SunriseCoder <sunrisecoder@gmail.com>
2018-10-07 14:22:28 +0300

    Wav-dump does not call popup window anymore

--- convert/win/wav-dump.bat
+++ convert/win/wav-dump.bat
@@ -1,3 +1,10 @@
 java -cp %CONVERT_HOME%\res\portal-integrations-0.0.2-SNAPSHOT.jar app.integrations.AudioDump %1 %2 "m"
-img-add-to-html.sh %2
+
+rem img-add-to-html.sh %2
+
+setlocal enabledelayedexpansion
+set RAW_URL=%2
+set SAFE_URL=%RAW_URL:"=%
+echo ^<img src="%SAFE_URL%" /^>^<br /^> >> index.html
+
 Echo Wav dump of %1 is done
