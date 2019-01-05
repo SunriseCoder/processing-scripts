7d2706ac90368a2c2275dd6dffafb9825094e7d8
SunriseCoder <sunrisecoder@gmail.com>
2019-01-05 19:13:05 +0300

    Bugfix: img-resize - output filename format

--- convert/win/img-resize.bat
+++ convert/win/img-resize.bat
@@ -3,4 +3,4 @@ echo Resizing %1 to "%2"x%3
 set filename=%1
 set w=%2
 set h=%3
-ffmpeg -i %filename% -vf "scale=%w%:%h%" resized-%w%%h%-%filename%
+ffmpeg -i %filename% -vf "scale=%w%:%h%" resized-%w%x%h%-%filename%
