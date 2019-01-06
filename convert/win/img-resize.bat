c17203f1217611442606639c19ff4dabeba5a0be
SunriseCoder <sunrisecoder@gmail.com>
2019-01-06 20:21:03 +0300

    img-resize - added file extension if set

--- convert/win/img-resize.bat
+++ convert/win/img-resize.bat
@@ -1,6 +1,7 @@
-echo Resizing %1 to "%2"x%3
+echo Resizing %1 to "%2"x%3 %4
 
 set filename=%1
 set w=%2
 set h=%3
-ffmpeg -i %filename% -vf "scale=%w%:%h%" resized-%w%x%h%-%filename%
+set ext=%4
+ffmpeg -i %filename% -vf "scale=%w%:%h%" resized-%w%x%h%-%filename%%ext%
