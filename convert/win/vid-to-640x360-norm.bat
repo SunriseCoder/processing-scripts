af4623195d7857bd576094a431f33a8f54766cb3
SunriseCoder <sunrisecoder@gmail.com>
2019-07-30 18:41:04 +0300

    vid-to-640x360 adjustments

--- convert/win/vid-to-640x360-norm.bat
+++ convert/win/vid-to-640x360-norm.bat
@@ -1,4 +1,5 @@
-set wave_name=%1.wav
+set wave_name=%~n1.wav
+set wave_norm_name=%~n1_norm.wav
 
 ffmpeg -i %1 -vn -ac 1 %wave_name%
 echo Unpacking of %wave_name% is done
@@ -8,7 +9,7 @@ echo Normalization of %wave_name% is done
 
 ffmpeg ^
 	-i %1 ^
-	-i %wave_name%_norm.wav ^
+	-i %wave_norm_name% ^
 	-map 0:0 ^
 	-map 1:0 ^
 	-c:v libx264 ^
@@ -22,6 +23,6 @@ echo Scaling for %1 is done
 echo deleting temporary files
 
 del %wave_name%
-del %wave_name%_norm.wav
+del %wave_norm_name%
 
 echo Scaling of %1 is done
--- convert/win/vid-to-640x360.bat
+++ convert/win/vid-to-640x360.bat
@@ -6,11 +6,5 @@ ffmpeg ^
 	-c:a aac ^
 	-q:a 64k ^
 	%1_scaled.mp4
-echo Scaling for %1 is done
-
-echo deleting temporary files
-
-del %wave_name%
-del %wave_name%_norm.wav
 
 echo Scaling of %1 is done
