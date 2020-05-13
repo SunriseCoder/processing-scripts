1b93ef6763c3bf6ef2e18736f5f93d8154f0a450
SunriseCoder <sunrisecoder@gmail.com>
2020-05-13 12:48:36 +0300

    vid-compress.bat - added some quality parameters

--- convert/win/vid-compress.bat
+++ convert/win/vid-compress.bat
@@ -4,8 +4,8 @@ set compressed_name=%~n1_compressed%~x1
 
 ffmpeg ^
 	-i %1 ^
-	-c:v libx264 -crf 23 ^
-	-c:a copy ^
+	-c:v libx264 -crf 23 -video_track_timescale 90000 -vsync vfr -r 25 ^
+	-c:a aac -vbr 3 ^
 	"%compressed_name%"
 
 echo Compressing of %1 is done
