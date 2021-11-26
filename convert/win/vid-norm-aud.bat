4971e22b817a90c23dd6649c5e77f06c163e3db0
SunriseCoder <sunrisecoder@gmail.com>
2021-11-26 13:01:56 +0300

    Changed: convert/win/vid-norm-aud.bat - limited audio channel number as 2, delete tmp file, added some debug info

--- convert/win/vid-norm-aud.bat
+++ convert/win/vid-norm-aud.bat
@@ -8,12 +8,32 @@ set wave_name=%~n1.wav
 set norm_wave_name=%~n1_norm.wav
 set norm_video_name=%~n1_norm%~x1
 
+set tmp_file=tmp.txt
+
+Rem Getting audio channel number
+ffprobe -v error -select_streams a:0 -show_entries stream=channels -of csv=p=0 %1 > %tmp_file%
+set /p source_channel_number=<%tmp_file%
+echo source_channel_number is %source_channel_number%
+
+set channel_number=%source_channel_number%
 if NOT "%~2"=="" (
 	set channel_number_parameters= -ac %2
+) else (
+	if %channel_number% gtr 2 (
+		Rem norm.bat doesn't supports mono or stereo only.
+		Rem If it was extended, please delete this fix
+		set channel_number=2
+	)
 )
+set channel_number_parameters= -ac %channel_number%
+
+echo channel_number is %channel_number%
+echo channel_number_parameters is %channel_number_parameters%
 
 Rem 1. Extracting Wav-file
 	ffmpeg -i %1 -vn%channel_number_parameters% "%wave_name%"
+
+
 	echo Unpacking of "%wave_name%" is done
 
 Rem 2. Calling Normalization Script
@@ -36,5 +56,6 @@ Rem 4. Deleting Temporary Files
 
 	del "%wave_name%"
 	del "%norm_wave_name%"
+	del %tmp_file%
 
 echo Normalization of audio track for %1 is done
