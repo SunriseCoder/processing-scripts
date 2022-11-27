330a5af57235b70d5330907d018f8f0e4ed28925
SunriseCoder <sunrisecoder@gmail.com>
2022-11-28 01:11:55 +0300

    Added custom FPS to v2

--- convert/res/videos/v2/generate-videos.bat
+++ convert/res/videos/v2/generate-videos.bat
@@ -1,1 +1,5 @@
 java -cp ..\..\jars\script-tools-0.0.1-SNAPSHOT-jar-with-dependencies.jar video.VideoResolutionGeneratorApp original generated target-resolutions.json
+
+echo All files has been generated
+
+pause
