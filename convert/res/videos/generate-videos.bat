13ad359a308fce88efdcb03128ffdfc80f0ba25d
SunriseCoder <sunrisecoder@gmail.com>
2022-12-19 20:22:24 +0300

    Added pause to generate-videos.bat

--- convert/res/videos/generate-videos.bat
+++ convert/res/videos/generate-videos.bat
@@ -1,1 +1,4 @@
 java -cp ..\jars\script-tools-0.0.1-SNAPSHOT-jar-with-dependencies.jar video.VideoResolutionGeneratorApp original generated target-resolutions.json
+
+echo Generating is done
+pause
--- convert/res/videos/v2/generate-videos.bat
+++ convert/res/videos/v2/generate-videos.bat
@@ -1,5 +1,4 @@
 java -cp ..\..\jars\script-tools-0.0.1-SNAPSHOT-jar-with-dependencies.jar video.VideoResolutionGeneratorApp original generated target-resolutions.json
 
-echo All files has been generated
-
+echo Generating is done
 pause
