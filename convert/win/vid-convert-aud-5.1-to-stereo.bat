7192c77e95cb4cd2986aa609d3f63dcb7bd69c2f
SunriseCoder <sunrisecoder@gmail.com>
2023-02-08 19:26:09 +0300

    Fixed: vid-convert-aud-5.1-to-stereo.bat

--- convert/win/vid-convert-aud-5.1-to-stereo.bat
+++ convert/win/vid-convert-aud-5.1-to-stereo.bat
@@ -2,20 +2,9 @@ Rem Usage: vid-convert-aud-5.1-to-stereo.bat <filename>
 
 Rem variables
 set resource_path_escaped=%CONVERT_HOME:\=\\%\\res
-set tmp_file=tmp.txt
-set silent_suffix=
-if "%2"=="silent" set silent_suffix=silent
-
-set wave_name=%~n1.wav
-set file_ext=%~x1
-set norm_wave_name=%~n1_norm.wav
-
-set video_with_logo_name=%~n1_logo.mp4
 
 set video_tmp_name=%~n1_tmp.mp4
-set video_ready_name=%~n1_ready.mp4
-
-del %tmp_file%
+set video_ready_name=%~n1_stereo.mp4
 
 ffmpeg ^
 	-i %1 ^
@@ -25,21 +14,21 @@ ffmpeg ^
 	-c:a aac -ar 48000 -ac 2 -vbr 3 ^
 	"%video_tmp_name%"
 
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 goto error /b %errorlevel%
 
 move "%video_tmp_name%" "%video_ready_name%"
 
-if %errorlevel% neq 0 exit /b %errorlevel%
+if %errorlevel% neq 0 goto error /b %errorlevel%
 
 echo Processing of %1 is done
 echo ============================================================
 
-:exit
-echo Completed successfully
-pause
-exit
+:success
+echo === Completed successfully ===
+goto end
 
 :error
-echo Completed with Errors!!!
-pause
-exit
+echo === Completed with Errors!!! ===
+goto end
+
+:end
