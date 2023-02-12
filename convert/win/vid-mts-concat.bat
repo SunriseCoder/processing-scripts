cb3c63d06573bf2c66dba504428f32104ce65d0b
SunriseCoder <sunrisecoder@gmail.com>
2023-02-12 18:35:58 +0300

    Added: vid-mts-concat.bat

---
+++ convert/win/vid-mts-concat.bat
@@ -0,0 +1,7 @@
+Rem Concats all MTS-files in the current folder
+
+java -cp %CONVERT_HOME%/res/jars/script-tools-0.0.1-SNAPSHOT-jar-with-dependencies.jar files.ConcatMTSFiles
+
+echo MTS Concat is done
+
+pause
