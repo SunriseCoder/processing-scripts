d97b538b5e411a4eefbf489e366f80e62b370638
SunriseCoder <sunrisecoder@gmail.com>
2023-11-10 20:21:28 +0300

    Fixed: vid-compress-v2.bat - added intermediate temporary filename

--- convert/win/vid-compress-v2.bat
+++ convert/win/vid-compress-v2.bat
@@ -1,13 +1,14 @@
 Rem Compressing video to x264 CRF=23
 
-set compressed_name=%~n1_compressed%~x1
+set tmp_name=%~n1_compressed-tmp%~x1
+set out_name=%~n1_compressed%~x1
 
 ffmpeg ^
 	-i %1 ^
 	-c:v libx264 -crf 23 -video_track_timescale 90000 ^
 	-c:a aac -vbr 3 ^
-	"%compressed_name%"
+	"%tmp_name%"
+
+move "%tmp_name%" "%out_name%"
 
 echo Compressing of %1 is done
-
-Rem call vid-norm-aud.bat "%compressed_name%"
